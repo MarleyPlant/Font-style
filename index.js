a692142e2d7b9e19bc0ca8311b5882e78d2ec824
Marley Plant <marley@marleyplant.com>
2024-02-16 04:19:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const stylesPath = join(__dirname, 'styles.json')
 const fontStyles = JSON.parse(readFileSync(stylesPath, 'utf8'))
 
 const getAllFonts = Object.keys(fontStyles)
+const resetASCII = '\x1b[0m'
+const setColorASCII = '\x1b['
+// https://i0.wp.com/davidlozzi.com/wp-content/uploads/2021/03/image-8.png?w=243&ssl=1
+const colors = {
+    red: "31",
+    green: "32
+}
+
 
 const fontStyle = (text, font = 'black-square') => {
     if (!fontStyles.hasOwnProperty(font)) {
